58e6aa7df841030cd62a0110cf6c7c1d35d4093a
steven85cr <112594682+steven85cr@users.noreply.github.com>
2026-01-14 19:11:14 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = '4x4-calendario-v1';
+const ASSETS_TO_CACHE = [
+  './calendario.html',
+  './manifest.json',
+  './img/logo.webp',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap'
+];
+
+// Instalación y cacheo
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Activación y limpieza de caches viejos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Interceptar peticiones (Offline First)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
